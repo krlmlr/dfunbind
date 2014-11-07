e2e38d4fc24aae575704c63ff024297f76d21ab5
Kirill Müller <kirill.mueller@ivt.baug.ethz.ch>
2014-11-07 11:08:36 +0100

    add test infrastructure

---
+++ README.md
@@ -0,0 +1,1 @@
+# dfunbind [![Build Status](https://snap-ci.com/krlmlr/dfunbind/branch/master/build_image)](https://snap-ci.com/krlmlr/dfunbind/branch/master)
---
+++ tests/testthat/test-unbind.R
@@ -0,0 +1,12 @@
+context('Unbind')
+
+test_that('Unbind creates files and directories', {
+  my_subdir <- "subdir/for/data"
+  on.exit(unlink(my_subdir, recursive = TRUE))
+  unbind_one(iris, "iris", my_subdir)
+
+  expect_true(file.exists(my_subdir))
+  expect_true(file.info(my_subdir)$isdir)
+  files <- dir(my_subdir)
+  expect_equal(length(files), ncol(iris))
+})
