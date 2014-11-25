90a1b679a08331d2128e0a8903e7faf748843448
Kirill Müller <kirill.mueller@ivt.baug.ethz.ch>
2014-11-25 16:14:23 +0100

    add dataset-specific test for mtcars

---
+++ tests/testthat/test-unbind-mtcars.R
@@ -0,0 +1,22 @@
+context('Unbind mtcars')
+
+my_subdir <- "mtcars/subdir"
+on.exit(unlink(my_subdir, recursive = TRUE))
+unbind(mtcars, my_subdir)
+
+test_that('File name format for unbind', {
+  files <- dir(my_subdir)
+  expect_equal(files, c(sprintf("%.2d-%s.rds", seq_len(ncol(mtcars) + 1) - 1, c("rownames", colnames(mtcars))), DICT_FILENAME))
+})
+
+test_that('splice and row names', {
+  expect_false(exists(file.path(my_subdir, "0-rownames.rds")))
+})
+
+test_that('splice and $', {
+  mtcars_splice <- splice(my_subdir)
+  expect_equal(mtcars_splice$mpg, mtcars$mpg)
+  expect_equal(mtcars_splice$mp, mtcars$mpg)
+  expect_equal(mtcars_splice$di, mtcars$di)
+  expect_error(mtcars_splice$bla)
+})
